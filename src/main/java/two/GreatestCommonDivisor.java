f9048b5f42e30b68e5f6decd2bfb5e63edeeb71a
Ilya Demidenko <demidenko.ilya@gmail.com>
2022-04-26 22:31:51 +0300

    Calculate greatest common divisor.

---
+++ src/main/java/two/GreatestCommonDivisor.java
@@ -0,0 +1,26 @@
+package two;
+
+import java.util.Scanner;
+
+public class GreatestCommonDivisor {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+
+    if (b > a) {
+      int temp = a;
+      a = b;
+      b = temp;
+    }
+
+    while (b != 0) {
+      int mod = a % b;
+      a = b;
+      b = mod;
+    }
+
+    System.out.println(a);
+  }
+}
