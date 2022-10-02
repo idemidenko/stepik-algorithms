9fbc30f79785c5e515fc58004b7cd2f1b7b0cb1f
Ilya Demidenko <demidenko.ilya@gmail.com>
2022-10-02 18:35:06 +0300

    [CrackingCodeInterview] ArraysAndStrings - URLify string.

---
+++ src/main/java/crackingcodeinterview/one/one/ArraysAndStrings.java
@@ -0,0 +1,89 @@
+package crackingcodeinterview.one.one;
+
+import java.util.Arrays;
+
+public class ArraysAndStrings {
+
+  public static void main(String[] args) {
+    String str = "string11";
+    System.out.println(str + " is unique: " + isUniqueChars(str));
+
+    String s2 = "1string1";
+    System.out.println(str + " is permutation of " + s2 + ": " + isPermutation(str, s2));
+
+    String urlifyInput = " a  b      ";
+    System.out.println(
+        "urlify: input - [" + urlifyInput + "], result - [" + urlify(urlifyInput) + "]");
+  }
+
+  // nlogn
+  /**
+   * Is Unique: Implement an algorithm to determine if a string has all unique characters. What if
+   * you cannot use additional data structures?
+   */
+  private static boolean isUniqueChars(String str) { // string11
+    if (str == null) {
+      throw new IllegalArgumentException("Incoming string should not be null");
+    }
+    char[] chars = str.toCharArray(); // ['s' 0, 't' 1, 'r' 2, 'i' 3, 'n' 4, 'g' 5, '1' 6, '1' 7]
+    for (int i = 0; i < chars.length; i++) { // 0 | 1 | 2 | 3
+      char c = chars[i]; // 's' | t | r | i
+      for (int j = i + 1; j < chars.length; j++) { // 4 | 5 | 6 | 7
+        if (c == chars[j]) { // i == n | g | 1 | 1
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Check Permutation: Given two strings,write a method to decide if one is a permutation of the
+   * other.
+   */
+  private static boolean isPermutation(String s1, String s2) {
+    if (s1 == null || s2 == null) {
+      throw new IllegalArgumentException("Params should not be null. s1 = " + s1 + ", s2 = " + s2);
+    }
+
+    char[] s1Array = s1.toCharArray();
+    Arrays.sort(s1Array);
+    char[] s2Array = s2.toCharArray();
+    Arrays.sort(s2Array);
+    return Arrays.equals(s1Array, s2Array);
+  }
+
+  /**
+   * URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the
+   * string has sufficient space at the end to hold the additional characters,and that you are given
+   * the "true" length of the string. (Note: If implementing in Java,please use a character array so
+   * that you can perform this operation in place.)
+   * EXAMPLE
+   * Input:  "Mr John Smith ", 13
+   * Output: "Mr%20John%20Smith"
+   */
+  private static String urlify(String s) {
+    char[] chars = s.toCharArray();
+    boolean skipSpace = true;
+
+    for (int i = chars.length - 1, j = chars.length - 1;
+        i >= 0;
+        i--) { // 10 10 | 9 10 | ... | 4 10 | 3 9 | 2 6 | 1 3 | 0 2
+      char c = chars[i]; // b | ' ' | ' ' | a |
+
+      if (c == ' ') {
+        if (skipSpace) {
+          continue;
+        }
+        // replace space
+        chars[j--] = '0';
+        chars[j--] = '2';
+        chars[j--] = '%';
+      } else {
+        skipSpace = false;
+        chars[j--] = c; // b | a
+      }
+    }
+    return new String(chars);
+  }
+}
