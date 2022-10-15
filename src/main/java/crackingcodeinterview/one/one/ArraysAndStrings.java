26c44edba8ab28789f466b809cc22756457f8343
Ilya Demidenko <demidenko.ilya@gmail.com>
2022-10-15 21:05:39 +0300

    [CrackingCodeInterview] ArraysAndStrings - check if strings are one edit (or zero edits) away.

--- src/main/java/crackingcodeinterview/one/one/ArraysAndStrings.java
+++ src/main/java/crackingcodeinterview/one/one/ArraysAndStrings.java
@@ -18,10 +18,26 @@ public class ArraysAndStrings {
         "urlify: input - [" + urlifyInput + "], result - [" + urlify(urlifyInput) + "]");
 
     String palindrome = "aba";
-    System.out.println("String: [" + palindrome + "] isPalindromePermutation: [" + isPalindromePermutation(palindrome) + "]");
+    System.out.println(
+        "String: ["
+            + palindrome
+            + "] isPalindromePermutation: ["
+            + isPalindromePermutation(palindrome)
+            + "]");
     String palindromePermutation = "Tact Coa";
-    System.out.println("String: [" + palindromePermutation + "] isPalindromePermutation: [" + isPalindromePermutation(palindromePermutation) + "]");
+    System.out.println(
+        "String: ["
+            + palindromePermutation
+            + "] isPalindromePermutation: ["
+            + isPalindromePermutation(palindromePermutation)
+            + "]");
 
+    printIsOneEditAway("pale", "ple");
+    printIsOneEditAway("pales", "pale");
+    printIsOneEditAway("pale", "bale");
+    printIsOneEditAway("pale", "bake");
+    printIsOneEditAway("sale", "sale");
+    printIsOneEditAway("", "");
   }
 
   // nlogn
@@ -65,10 +81,8 @@ public class ArraysAndStrings {
    * URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the
    * string has sufficient space at the end to hold the additional characters,and that you are given
    * the "true" length of the string. (Note: If implementing in Java,please use a character array so
-   * that you can perform this operation in place.)
-   * EXAMPLE
-   * Input:  "Mr John Smith ", 13
-   * Output: "Mr%20John%20Smith"
+   * that you can perform this operation in place.) EXAMPLE Input: "Mr John Smith ", 13 Output:
+   * "Mr%20John%20Smith"
    */
   private static String urlify(String s) {
     char[] chars = s.toCharArray();
@@ -96,16 +110,13 @@ public class ArraysAndStrings {
   }
 
   /**
-   Palindrome Permutation: Given a string, write a function to check if it is a permutation of a
-   palindrome. A palindrome is a word or phrase that is the same forwards and backwards.
-   A permutation is a rearrangement of letters.
-   The palindrome does not need to be limited to just dictionary words.
-   EXAMPLE
-   Input: Tact Coa
-   Output: True (permutations: "taco cat", "atco cta", etc.)
-
-   Input: T   actC oa
-   Output: True (permutations: "t   acoc at", "a   tcoc ta", etc.)
+   * Palindrome Permutation: Given a string, write a function to check if it is a permutation of a
+   * palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A
+   * permutation is a rearrangement of letters. The palindrome does not need to be limited to just
+   * dictionary words. EXAMPLE Input: Tact Coa Output: True (permutations: "taco cat", "atco cta",
+   * etc.)
+   *
+   * <p>Input: T actC oa Output: True (permutations: "t acoc at", "a tcoc ta", etc.)
    */
   private static boolean isPalindromePermutation(String s) {
     if (s == null || s.length() < 3) {
@@ -114,7 +125,7 @@ public class ArraysAndStrings {
 
     s = s.replaceAll("\\s", "").toLowerCase();
 
-    if(isPalindrome(s)) {
+    if (isPalindrome(s)) {
       return false;
     }
     char[] chars = s.toCharArray();
@@ -139,7 +150,7 @@ public class ArraysAndStrings {
 
   private static boolean isPalindrome(String s) {
     char[] chars = s.toCharArray();
-    for(int i = 0, j = chars.length - 1; i < j; i++) {
+    for (int i = 0, j = chars.length - 1; i < j; i++) {
       if (chars[i] != chars[j--]) {
         return false;
       }
@@ -147,4 +158,62 @@ public class ArraysAndStrings {
     return true;
   }
 
+  /*
+  One Away: There are three types of edits that can be performed on strings: insert a character,
+  remove a character, or replace a character. Given two strings, write a function to check if
+  they are one edit (or zero edits) away.
+
+  EXAMPLE
+  pale, ple -> true
+  pales, pale -> true
+  pale, bale -> true
+  pale, bake -> false
+  */
+  public static boolean isOneEditAway(String s1, String s2) {
+    if (s1 == null || s2 == null) {
+      return false;
+    }
+
+    char[] chars1 = s1.toCharArray();
+    char[] chars2 = s2.toCharArray();
+
+    int lengthDiff = chars1.length - chars2.length;
+    boolean removeOperation = false;
+    if (lengthDiff == 1 || lengthDiff == -1) {
+      removeOperation = true;
+    } else if (lengthDiff != 0) {
+      return false;
+    }
+
+    int numberOfEdits = 0;
+    int maxNumberOfEdits = 1;
+    for (int i = 0, j = 0; i < chars1.length && j < chars2.length; i++, j++) {
+      char c1 = chars1[i];
+      char c2 = chars2[j];
+
+      if (c1 != c2) {
+        if (++numberOfEdits > maxNumberOfEdits) {
+          return false;
+        }
+
+        if (removeOperation) {
+          if (chars1.length > chars2.length) {
+            c1 = chars1[++i];
+          } else {
+            c2 = chars2[++j];
+          }
+          if (c1 != c2) {
+            return false;
+          }
+        }
+      }
+    }
+
+    return true;
+  }
+
+  private static void printIsOneEditAway(String s1, String s2) {
+    System.out.printf(
+        "isOneEditAway: s1 = [%s], s2 = [%s], result = [%s]%n", s1, s1, isOneEditAway(s1, s2));
+  }
 }
